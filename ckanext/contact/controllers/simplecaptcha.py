03222cd9c03cf13570a93480645ddeba9eb3bdf3
Teemu Erkkola <teemu.erkkola@gofore.com>
2016-01-27 13:10:28 +0200

    Remove debug logging

--- ckanext/contact/controllers/simplecaptcha.py
+++ ckanext/contact/controllers/simplecaptcha.py
@@ -6,7 +6,6 @@ log = logging.getLogger(__name__)
 def check_captcha(request):
     captcha_response_field = request.params.get('captcha_response_field', '')
     captcha_challenge_field = request.params.get('captcha_challenge_field')
-    log.warning("%s = %s" % (captcha_challenge_field, captcha_response_field))
     try:
         expected_result = sum(int(n) for n in captcha_challenge_field.split())
         received_result = int(captcha_response_field)
